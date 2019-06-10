5048fab5cc21373e2e77507084d5e72e2432e1f1
jason1466 <jason1466@users.noreply.github.com>
2019-06-10 16:04:41 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> ng-app="myapp">
+<head>
+  <title>Windowsoft Angular Material Business Services Theme</title>
+  <base href="/">
+  <?php wp_head();?>
+</head>
+<body>
