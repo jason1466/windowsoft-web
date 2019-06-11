8658c42b18a5875c8b4f9be4f30dff856237b0e6
jason1466 <jason1466@users.noreply.github.com>
2019-06-11 10:58:55 -0400

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> ng-app="myapp">
-<head>
-  <title>Windowsoft Angular Material Business Services Theme</title>
-  <base href="/">
-  <?php wp_head();?>
-</head>
-<body>
